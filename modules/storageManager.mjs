5c586ccb52ab214bad76cf8cd1f1bfa8e07673a6
dvnnyle <147186034+dvnnyle@users.noreply.github.com>
2024-10-05 06:05:25 +0200

    Update storageManager.mjs

--- modules/storageManager.mjs
+++ modules/storageManager.mjs
@@ -1,7 +1,19 @@
-
 import pg from "pg";
 
- 
+class DBManager {
+  #pool;
+
+
+}
+
+let connectionString =
+  process.env.ENVIRONMENT === "production"
+    ? process.env.DB_CONNECTIONSTRING_PROD
+    : process.env.DB_CONNECTIONSTRING_LOCAL;
+
+if (process.env.ENVIRONMENT === "production" && connectionString === undefined) {
+  throw new Error("You forgot the db connection string for production environment");
+}
 
 
 export default new DBManager(connectionString);
