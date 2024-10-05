7ebd889b9d8e67e58cd07483a65f07c06fbfd836
dvnnyle <147186034+dvnnyle@users.noreply.github.com>
2024-10-05 06:10:14 +0200

    Update user.mjs

--- modules/user.mjs
+++ modules/user.mjs
@@ -1,34 +1,8 @@
 import DBManager from "./storageManager.mjs";
 
 class User {
-  constructor() {
-    this.email = "";
-    this.pswHash = "";
-    this.name = "";
-    this.id = null;
-  }
 
-  async save() {
-    try {
-      if (this.id === null) {
-        return await DBManager.createUser(this);
-      } else {
-        return await DBManager.updateUser(this);
-      }
-    } catch (error) {
-      console.error('Error saving user:', error);
-      throw error; // Propagate the error for handling in the calling code
-    }
-  }
-
-  async delete() {
-    try {
-      await DBManager.deleteUser(this);
-    } catch (error) {
-      console.error('Error deleting user:', error);
-      throw error; // Propagate the error for handling in the calling code
-    }
-  }
+  
 }
 
 export default User;
